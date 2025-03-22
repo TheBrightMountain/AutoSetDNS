eae750ed6e8987e8c27b39418f84d5dae624eae0
ADMIN <minhson6a1@gmail.com>
2025-03-22 17:56:03 +0700

    Disable On-Batteries restrictions. Add administrator check and error handling to installation script

--- Source/Install.ps1
+++ Source/Install.ps1
@@ -1,3 +1,15 @@
+# Check if the script is running as administrator
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+  Add-Type -AssemblyName PresentationFramework
+  [System.Windows.MessageBox]::Show(
+    "This script must be run as an administrator!",
+    "Administrator Privileges Required",
+    [System.Windows.MessageBoxButton]::OK,
+    [System.Windows.MessageBoxImage]::Error
+  )
+  exit 1
+}
+
 $basePath = if ($PSScriptRoot) { $PSScriptRoot } else { [System.IO.Path]::GetDirectoryName([System.Diagnostics.Process]::GetCurrentProcess().MainModule.FileName) }
 $logPath = Join-Path $basePath "AutoSetDNS.log"
 $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
@@ -7,19 +19,26 @@ Remove-Item "$env:TEMP\AutoSetDNS-Installer.log" -Force -ErrorAction SilentlyCon
 "[$timestamp] [Installer] Starting install..." | Add-Content -Path $logPath -Encoding UTF8
 
 try {
-    $exePath = Join-Path $basePath "AutoSetDNS.exe"
-    $taskName = "AutoSetDNS"
-    $logName = "Microsoft-Windows-NetworkProfile/Operational"
-    $userName = "$env:UserDomain\$env:UserName"
+  $exePath = Join-Path $basePath "AutoSetDNS.exe"
+  $taskName = "AutoSetDNS"
+  $logName = "Microsoft-Windows-NetworkProfile/Operational"
+  $userName = "$env:UserDomain\$env:UserName"
 
-    if (!(Test-Path $exePath)) {
-        "[$timestamp] [Installer] AutoSetDNS.exe not found." | Add-Content -Path $logPath -Encoding UTF8
-        exit 1
-    }
+  if (!(Test-Path $exePath)) {
+    "[$timestamp] [Installer] AutoSetDNS.exe not found." | Add-Content -Path $logPath -Encoding UTF8
+    Add-Type -AssemblyName PresentationFramework
+    [System.Windows.MessageBox]::Show(
+      "AutoSetDNS.exe not found. Installation cannot proceed.",
+      "Installation Failed",
+      [System.Windows.MessageBoxButton]::OK,
+      [System.Windows.MessageBoxImage]::Error
+    )
+    exit 1
+  }
 
-    wevtutil set-log $logName /enabled:true
+  wevtutil set-log $logName /enabled:true
 
-    $xml = @"
+  $xml = @"
 <?xml version="1.0" encoding="UTF-16"?>
 <Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
   <Triggers>
@@ -47,6 +66,8 @@ try {
     <AllowStartOnDemand>true</AllowStartOnDemand>
     <Enabled>true</Enabled>
     <Hidden>true</Hidden>
+    <DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries>
+    <StopIfGoingOnBatteries>false</StopIfGoingOnBatteries>
   </Settings>
   <Actions Context="Author">
     <Exec>
@@ -56,13 +77,29 @@ try {
 </Task>
 "@
 
-    $xmlPath = "$env:TEMP\AutoSetDNS_Task.xml"
-    $xml | Out-File -Encoding Unicode $xmlPath
+  $xmlPath = "$env:TEMP\AutoSetDNS_Task.xml"
+  $xml | Out-File -Encoding Unicode $xmlPath
 
-    schtasks.exe /Create /TN $taskName /XML $xmlPath /F
-    Remove-Item $xmlPath -Force
+  schtasks.exe /Create /TN $taskName /XML $xmlPath /F
+  Remove-Item $xmlPath -Force
 
-    "[$timestamp] [Installer] Task '$taskName' registered successfully." | Add-Content -Path $logPath -Encoding UTF8
-} catch {
-    "[$timestamp] [Installer] ERROR: $_" | Add-Content -Path $logPath -Encoding UTF8
+  "[$timestamp] [Installer] Task '$taskName' registered successfully." | Add-Content -Path $logPath -Encoding UTF8
+
+  Add-Type -AssemblyName PresentationFramework
+  [System.Windows.MessageBox]::Show(
+    "The task '$taskName' was registered successfully.",
+    "Installation Successful",
+    [System.Windows.MessageBoxButton]::OK,
+    [System.Windows.MessageBoxImage]::Information
+  )
+}
+catch {
+  "[$timestamp] [Installer] ERROR: $_" | Add-Content -Path $logPath -Encoding UTF8
+  Add-Type -AssemblyName PresentationFramework
+  [System.Windows.MessageBox]::Show(
+    "An error occurred during installation: $_",
+    "Installation Error",
+    [System.Windows.MessageBoxButton]::OK,
+    [System.Windows.MessageBoxImage]::Error
+  )
 }
